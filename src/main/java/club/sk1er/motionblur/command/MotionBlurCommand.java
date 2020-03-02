1fa8f6bfca9f2084e088bbfc02de373a316673fa
asbyth <cyronize@gmail.com>
2020-03-02 14:53:38 -0500

    add mod info

--- src/main/java/club/sk1er/motionblur/command/MotionBlurCommand.java
+++ src/main/java/club/sk1er/motionblur/command/MotionBlurCommand.java
@@ -11,62 +11,65 @@ import net.minecraft.util.ResourceLocation;
 
 public class MotionBlurCommand extends CommandBase {
 
-    private final Minecraft mc = Minecraft.getMinecraft();
+  private final Minecraft mc = Minecraft.getMinecraft();
 
-    /**
-     * Gets the name of the command
-     */
-    @Override
-    public String getCommandName() {
-        return "motionblur";
-    }
+  /** Gets the name of the command */
+  @Override
+  public String getCommandName() {
+    return "motionblur";
+  }
 
-    /**
-     * Gets the usage string for the command.
-     */
-    @Override
-    public String getCommandUsage(ICommandSender sender) {
-        return "/" + getCommandName();
-    }
+  /** Gets the usage string for the command. */
+  @Override
+  public String getCommandUsage(ICommandSender sender) {
+    return "/" + getCommandName();
+  }
 
-    /**
-     * Callback when the command is invoked
-     */
-    @Override
-    public void processCommand(ICommandSender sender, String[] args) {
-        if (args.length == 0) {
-            MinecraftUtils.sendMessage(EnumChatFormatting.RED + "[MotionBlur]", " Usage: /motionblur <0 - 7>");
+  /** Callback when the command is invoked */
+  @Override
+  public void processCommand(ICommandSender sender, String[] args) {
+    if (args.length == 0) {
+      MinecraftUtils.sendMessage(
+          EnumChatFormatting.RED + "[MotionBlur]", " Usage: /motionblur <0 - 7>");
+    } else {
+      int amount = Integer.parseInt(args[0]);
+      if (amount >= 0 && amount <= 7) {
+        if (MotionBlur.instance.isFastRenderEnabled()) {
+          MinecraftUtils.sendMessage(
+              EnumChatFormatting.RED + "[MotionBlur]",
+              " Motion Blur is not compatible with OptiFine's Fast Render.");
         } else {
-            int amount = Integer.parseInt(args[0]);
-            if (amount >= 0 && amount <= 7) {
-                if (MotionBlur.instance.isFastRenderEnabled()) {
-                    MinecraftUtils.sendMessage(EnumChatFormatting.RED + "[MotionBlur]", " Motion Blur is not compatible with OptiFine's Fast Render.");
-                } else {
-                    if (mc.entityRenderer.getShaderGroup() != null) {
-                        mc.entityRenderer.getShaderGroup().deleteShaderGroup();
-                    }
+          if (mc.entityRenderer.getShaderGroup() != null) {
+            mc.entityRenderer.getShaderGroup().deleteShaderGroup();
+          }
 
-                    if (amount != 0) {
-                        BlurConfig.motionBlur = true;
-                        BlurConfig.blurAmount = amount;
-                        mc.entityRenderer.loadShader(new ResourceLocation("motionblur", "motionblur"));
-                        mc.entityRenderer.getShaderGroup().createBindFramebuffers(mc.displayWidth, mc.displayHeight);
-                        MinecraftUtils.sendMessage(EnumChatFormatting.RED + "[MotionBlur]", " Motion Blur enabled with amount " + amount + ".");
-                    } else {
-                        BlurConfig.motionBlur = false;
-                        MinecraftUtils.sendMessage(EnumChatFormatting.RED + "[MotionBlur]", " Motion Blur disabled.");
-                    }
+          if (amount != 0) {
+            BlurConfig.motionBlur = true;
+            BlurConfig.blurAmount = amount;
+            mc.entityRenderer.loadShader(new ResourceLocation("motionblur", "motionblur"));
+            mc.entityRenderer
+                .getShaderGroup()
+                .createBindFramebuffers(mc.displayWidth, mc.displayHeight);
+            MinecraftUtils.sendMessage(
+                EnumChatFormatting.RED + "[MotionBlur]",
+                " Motion Blur enabled with amount " + amount + ".");
+          } else {
+            BlurConfig.motionBlur = false;
+            MinecraftUtils.sendMessage(
+                EnumChatFormatting.RED + "[MotionBlur]", " Motion Blur disabled.");
+          }
 
-                    MotionBlur.instance.saveConfig();
-                }
-            } else {
-                MinecraftUtils.sendMessage(EnumChatFormatting.RED + "[MotionBlur]", " Invalid blur amount, 0 - 7.");
-            }
+          MotionBlur.instance.saveConfig();
         }
+      } else {
+        MinecraftUtils.sendMessage(
+            EnumChatFormatting.RED + "[MotionBlur]", " Invalid blur amount, 0 - 7.");
+      }
     }
+  }
 
-    @Override
-    public int getRequiredPermissionLevel() {
-        return -1;
-    }
+  @Override
+  public int getRequiredPermissionLevel() {
+    return -1;
+  }
 }
--- src/main/java/club/sk1er/motionblur/resource/MotionBlurResourceManager.java
+++ src/main/java/club/sk1er/motionblur/resource/MotionBlurResourceManager.java
@@ -1,31 +1,30 @@
 package club.sk1er.motionblur.resource;
 
+import java.util.List;
+import java.util.Set;
 import net.minecraft.client.resources.FallbackResourceManager;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.client.resources.data.IMetadataSerializer;
 import net.minecraft.util.ResourceLocation;
 
-import java.util.List;
-import java.util.Set;
-
 public class MotionBlurResourceManager extends FallbackResourceManager implements IResourceManager {
-    public MotionBlurResourceManager(IMetadataSerializer frmMetadataSerializerIn) {
-        super(frmMetadataSerializerIn);
-    }
+  public MotionBlurResourceManager(IMetadataSerializer frmMetadataSerializerIn) {
+    super(frmMetadataSerializerIn);
+  }
 
-    @Override
-    public Set<String> getResourceDomains() {
-        return null;
-    }
+  @Override
+  public Set<String> getResourceDomains() {
+    return null;
+  }
 
-    @Override
-    public IResource getResource(ResourceLocation location) {
-        return new MotionBlurResource();
-    }
+  @Override
+  public IResource getResource(ResourceLocation location) {
+    return new MotionBlurResource();
+  }
 
-    @Override
-    public List<IResource> getAllResources(ResourceLocation location) {
-        return null;
-    }
+  @Override
+  public List<IResource> getAllResources(ResourceLocation location) {
+    return null;
+  }
 }
