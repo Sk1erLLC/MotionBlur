dca60ec389629e27b87a32a6279b15a4ff8a35ee
asbyth <cyronize@gmail.com>
2021-05-25 07:41:51 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/motionblur/tweaker/MotionBlurTweaker.java
+++ src/main/java/club/sk1er/motionblur/tweaker/MotionBlurTweaker.java
@@ -1,9 +1,9 @@
 package club.sk1er.motionblur.tweaker;
 
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
 @SuppressWarnings("unused")
-public class MotionBlurTweaker extends EssentialTweaker {
+public class MotionBlurTweaker extends EssentialSetupTweaker {
     public MotionBlurTweaker() {
         super(new String[]{});
     }
